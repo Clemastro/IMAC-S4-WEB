e7f4ad466a12c8792f9c4950ce7982cd144f66a5
Clemastro <clement_bourhis@hotmail.fr>
2022-03-03 12:07:06 +0100

    vesselCard v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VesselCard vesselName="Test"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import getVesselsData from './services/api/vesselAPI.js'
+import VesselCard from './components/vesselCard.vue'
+
+getVesselsData()
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VesselCard,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #131A21;
+}
+
+body{
+  margin: 0%;
 }
 </style>
---
+++ src/components/vesselCard.vue
@@ -0,0 +1,77 @@
+<!--Card with information about a ship-->
+<template>
+    <div class="vesselCard">
+        <!--Name of the ship-->
+        <h1>{{vesselName}}</h1>
+
+        <!--Picture of the ship-->
+        <div  class="vesselImg">
+            <!--Categorie(s) of the ship-->
+            <div class="vesselCardCategories">
+                <!--Status-->
+                <button class="vesselCardCategoriesStatus">{{vesselStatus}}</button>
+
+                <!--Type-->
+                <button class="vesselCardCategoriesType">{{vesselType}}</button>
+
+                <!--Focus-->
+                <button class="vesselCardCategoriesFocus">{{vesselFocus}}</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VesselCard',
+    props:{
+        vesselName: String,
+        vesselImg: String,
+        vesselStatus: String,
+        vesselType: String,
+        vesselFocus: String,
+    }
+}
+</script>
+
+<style scoped>
+.vesselCard{
+    width: 30%;
+    aspect-ratio: 3/2;
+
+    display:  flex;
+    flex-direction: column;
+}
+
+h1 {
+    height: 15%;
+    padding-left: 2%;
+    padding-top: 2%;
+    margin: 0%;
+
+    font-size: 2vw;
+
+    font-family : sans-serif;
+    background: #676F54;
+    color: white;
+}
+
+.vesselImg{
+    width: 100%;
+    height: 85%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    background: black;
+}
+
+.vesselCardCategories{
+    width: 50%;
+    background: rgb(0, 255, 0);
+}
+
+button{
+    width: 30%;
+    background: none;
+}
+</style>
