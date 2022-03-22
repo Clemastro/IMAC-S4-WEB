e0c9d032982ef822eabf81aaa5e227ac14c2d42a
Clemastro <clement_bourhis@hotmail.fr>
2022-03-22 12:37:23 +0100

    API and Gallery Fixed

--- src/components/vesselGalery.vue
+++ src/components/vesselGalery.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="vesselGalery">
-        <!--<VesselCard  :vesselData="vesselsData[0]"/>-->
-        <!--<VesselCard  
-            :vesselDataName="vesselsData.data[0].name" 
-            :vesselDataImg="vesselsData.data[0].media[0].images.background_blur"
-            :vesselDataStatus="vesselsData.data[0].production_status"
-            :vesselDataType="vesselsData.data[0].type"
-            :vesselDataFocus="vesselsData.data[0].focus"
-            />-->
-        <VesselCard v-for="vessel in vesselsData" :key="vessel.id"  :vesselDataName="vessel.name"/>
-        <p v-for="vessel in vesselsData" :key="vessel.id">he</p>
+        <VesselCard v-for="vessel in vesselsData" 
+            :key="vessel.id"  
+            :vesselDataName="vessel.name"
+            :vesselDataImg="vessel.media[0].images.background_blur"
+            :vesselDataStatus="vessel.production_status"
+            :vesselDataType="vessel.type"
+            :vesselDataFocus="vessel.focus"
+            />
     </div>
 </template>
 
@@ -32,6 +30,6 @@ export default {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-column-gap: 2%;
-    grid-row-gap: 2%;
+    grid-row-gap: 10px;
 }
 </style>
--- src/services/api/vesselAPI.js
+++ src/services/api/vesselAPI.js
@@ -1,11 +1,9 @@
 const getVesselsData = async function() {
     const response = await fetch("https://api.starcitizen-api.com/8b322b4bdc142d7bb9002e006d0f1234/v1/cache/ships")
-    const response2 = await fetch("https://valorant-api.com/v1/agents")
     if (response.status == 200){
         const data = await response.json()
         console.log(await data)
-        console.log(await response2.json())
-        return await data.data
+        return await data.data.filter(vessel => vessel != null)
     }
     else{
         new Error(response.statusText)
